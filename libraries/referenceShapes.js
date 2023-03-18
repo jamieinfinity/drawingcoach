a045a9a9e6d7f1e0dc7a538c1a530984fde60cbd
Jamie Williams <jamieinfinity@mac.com>
2023-03-18 16:09:32 -0400

    started abstracting shapes into classes; evaluate line with several scorign criteria which are displayed below canvas

--- libraries/referenceShapes.js
+++ libraries/referenceShapes.js
@@ -1,41 +1,143 @@
 
 const styleParameters = {
-    reference: {
+    normal: {
         lineColor: 0,
+        lineOpacity: 255,
         lineThickness: 4,
     },
-    compare: {
+    reference: {
         lineColor: 200,
+        lineOpacity: 255,
+        lineThickness: 4,
+    },
+    fit: {
+        lineColor: "#e71d36",
+        lineOpacity: 255,
         lineThickness: 4,
     }
 }
+const scoreWeights = {
+    location: 2,
+    length: 1,
+    angle: 1,
+    smoothness: 0.5
+}
+
+function fitLineToPoints(points) {
+    // see https://en.wikipedia.org/wiki/Deming_regression
+    let n = points.length;
+    let meanX = 0;
+    let meanY = 0;
+    for (let i = 0; i < n; i++) {
+        meanX += points[i].x;
+        meanY += points[i].y;
+    }
+    meanX /= n;
+    meanY /= n;
+
+    let covXX = 0;
+    let covXY = 0;
+    let covYY = 0;
+    for (let i = 0; i < n; i++) {
+        let xDiff = points[i].x - meanX;
+        let yDiff = points[i].y - meanY;
+        covXX += xDiff * xDiff;
+        covXY += xDiff * yDiff;
+        covYY += yDiff * yDiff;
+    }
+    covXX /= n;
+    covXY /= n;
+    covYY /= n;
+
+    let diff = covYY - covXX;
+    let beta1 = (diff + sqrt(diff*diff + 4*covXY*covXY)) / (2 * covXY);
+    let beta0 = meanY - beta1 * meanX;
+
+    function lineFunctin(p) {
+        let x = p.x;
+        let y = p.y;
+        let xprime = x + beta1*(y - beta0 - beta1*x)/(1 + beta1*beta1);
+        let yprime = beta0 + beta1*xprime;
+        return {x: xprime, y: yprime};
+    }
+    let pstart = lineFunctin({x:points[0].x, y:points[0].y});
+    let pend = lineFunctin({x:points[n-1].x, y:points[n-1].y});
+
+    // find the RMSE by using lineFunction to find the distance from each point to the line
+    let rmse = 0;
+    for (let i = 0; i < n; i++) {
+        let p = points[i];
+        let pprime = lineFunctin(p);
+        rmse += dist(p.x, p.y, pprime.x, pprime.y);
+    }
+    rmse /= n;
+
+    return { line: [pstart, pend], rmse:rmse }
+}
 
 class CurveSimpleLine {
-    constructor(canvas, points=[]) {
+    constructor(points = []) {
         this.points = points;
-        if (this.points.length == 0) {
-            this.makeRandomCurve(canvas);
-        }
+        this.rmse = null;
     }
-    makeRandomCurve(canvas) {
-        let x1 = random(0, canvas.width);
-        let y1 = random(0, canvas.height);
-        let x2 = random(0, canvas.width);
-        let y2 = random(0, canvas.height);
-        this.points = [{x: x1, y: y1}, {x: x2, y: y2}];
+    draw(canvas, style = "normal") {
+        let c = color(styleParameters[style].lineColor);
+        c.setAlpha(styleParameters[style].lineOpacity);
+        canvas.stroke(c);
+        canvas.strokeWeight(styleParameters[style].lineThickness);
+        canvas.line(this.points[0].x, this.points[0].y, this.points[1].x, this.points[1].y);
+    }
+    updateWithRandomCurve(canvas) {
+        let x1, x2, y1, y2;
+        let xmin = canvas.width / 10;
+        let xmax = canvas.width - canvas.width / 10;
+        let ymin = canvas.height / 10;
+        let ymax = canvas.height - canvas.height / 10;
+        let len = 0;
+        while (len < canvas.width / 3) {
+            x1 = random(xmin, xmax);
+            y1 = random(ymin, ymax);
+            x2 = random(xmin, xmax);
+            y2 = random(ymin, ymax);
+            len = dist(x1, y1, x2, y2);
+        }
+        this.points = [{ x: x1, y: y1 }, { x: x2, y: y2 }];
+        this.rmse = null;
+    }
+    updateWithFitToDrawing(drawingPoints) {
+        let { line, rmse } = fitLineToPoints(drawingPoints);
+        this.points = line;
+        this.rmse = rmse;
     }
     getCurveParameters() {
         let x = (this.points[0].x + this.points[1].x) / 2;
         let y = (this.points[0].y + this.points[1].y) / 2;
-        let angle = atan2(this.points[1].y - this.points[0].y, this.points[1].x - this.points[0].x);
-        angle = angle * 180 / PI;
+        let dx = (this.points[1].x - this.points[0].x);
+        let dy = (this.points[1].y - this.points[0].y);
+        let slope = dy / dx;
+        let angle = degrees(atan(-1 / slope));
         let length = dist(this.points[0].x, this.points[0].y, this.points[1].x, this.points[1].y);
-        return {centerPosition: {x: x, y: y}, length: length, angle: angle};
+        return { centerPosition: { x: x, y: y }, length: length, angle: angle };
     }
-    draw(canvas, style="reference") {
-        canvas.stroke(styleParameters[style].lineColor);
-        canvas.strokeWeight(styleParameters[style].lineThickness);
-        canvas.line(this.points[0].x, this.points[0].y, this.points[1].x, this.points[1].y);
+    getSimilarityScores(curve, canvas) {
+        let parameters1 = this.getCurveParameters();
+        let parameters2 = curve.getCurveParameters();
+        let distance = dist(parameters1.centerPosition.x, parameters1.centerPosition.y, parameters2.centerPosition.x, parameters2.centerPosition.y);
+        let angleDifference = abs(parameters1.angle - parameters2.angle);
+        let lengthDifference = abs(parameters1.length - parameters2.length);
+        console.log("distance: " + distance + ", angleDifference: " + angleDifference + ", lengthDifference: " + lengthDifference, ", rmse: " + curve.rmse);
+
+        // find a similarity scores, which is a number between 0 and 1
+        let decimals = 1;
+        let locationScore = round(10*Math.exp(-distance * distance / (canvas.width * canvas.width / 300)), decimals);
+        let angleScore = round(10*Math.exp(-angleDifference * angleDifference / (10 * 10)), decimals);
+        let lengthScore = round(10*Math.exp(-lengthDifference * lengthDifference / (parameters1.length * parameters1.length / 100)), decimals);
+        let smoothnessScore = round(10*Math.exp(-curve.rmse / (parameters1.length / 30)), decimals);
+        let overallScore = round((locationScore 
+                                + angleScore 
+                                + lengthScore 
+                                + smoothnessScore) / 4, decimals, decimals);
+        return { overall: overallScore, location: locationScore, angle: angleScore, length: lengthScore, smoothness: smoothnessScore };
     }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,21 @@
 const canvaseBorderWidth = 2;
 const canvaseBorderOffset = 2;
 
+const similarityScoreKeys = [
+  "location",
+  "length",
+  "angle",
+  "smoothness",
+  "overall"
+];
+const similarityScoreLabels = {
+  location: "Location",
+  length: "Length",
+  angle: "Orientation",
+  smoothness: "Smoothness",
+  overall: "Overall"
+};
+
 let canvasWidth;
 let canvasHeight;
 let canvasL;
@@ -16,18 +31,21 @@ let drawingPoints = [];
 
 
 function setup() {
-  canvasWidth = windowWidth/2;
-  canvasHeight = windowWidth/2;
+  canvasWidth = windowWidth / 2;
+  canvasHeight = windowWidth / 2;
   createCanvas(canvasWidth * 2, canvasHeight);
 
   canvasL = createGraphics(canvasWidth, canvasHeight);
   canvasR = createGraphics(canvasWidth, canvasHeight);
 
   nextButton = createButton('Next');
-  nextButton.size(100, 50);
+  nextButton.size(80, 40);
   nextButton.mousePressed(nextButtonPressed);
+  nextButton.position(canvasWidth - 90, canvasHeight + 10);
 
-  reference = new CurveSimpleLine(canvasL);
+  reference = new CurveSimpleLine();
+  reference.updateWithRandomCurve(canvasL);
+  fitToDrawing = new CurveSimpleLine();
 }
 
 function nextButtonPressed() {
@@ -36,16 +54,18 @@ function nextButtonPressed() {
   canvasL.background(255);
   canvasR.reset();
   canvasR.background(255);
-  reference = new CurveSimpleLine(canvasL);
+  resetScoreDiv();
+
+  reference.updateWithRandomCurve(canvasL);
   showReference = false;
   showFitToDrawing = false;
   drawingPoints = [];
 }
 
 function windowResized() {
-  canvasWidth = windowWidth/2;
-  canvasHeight = windowWidth/2;  
-  resizeCanvas(windowWidth, windowWidth);
+  canvasWidth = windowWidth / 2;
+  canvasHeight = windowWidth / 2;
+  resizeCanvas(windowWidth, canvasHeight);
 }
 
 function touchStarted(event) {
@@ -55,7 +75,8 @@ function touchStarted(event) {
 
   // Start a new line
   drawingPoints = [];
-  drawingPoints.push(createVector(mouseX-canvasWidth, mouseY));}
+  drawingPoints.push(createVector(mouseX - canvasWidth, mouseY));
+}
 
 function mousePressed() {
   // only do something if mouse is within the canvas
@@ -66,81 +87,83 @@ function mousePressed() {
 
     // Start a new line
     drawingPoints = [];
-    drawingPoints.push(createVector(mouseX-canvasWidth, mouseY));
+    drawingPoints.push(createVector(mouseX - canvasWidth, mouseY));
   }
 }
 
 function mouseDragged() {
   // Add a new point to the line
-  drawingPoints.push(createVector(mouseX-canvasWidth, mouseY));
+  drawingPoints.push(createVector(mouseX - canvasWidth, mouseY));
 }
 
 function mouseReleased() {
   // only do something if mouse is within the canvas
   if (mouseY < canvasHeight) {
     showReference = true;
-    showFitToDrawing = false;
+    showFitToDrawing = true;
 
-  // // fit line to points
-  // [a, b] = linearRegressionFit(drawingPoints);
-  // fitToDrawing = [0, b, canvasWidth, a * canvasWidth + b];
-  // // get line parameters
-  // fitLineParameters = getLineParameters(fitToDrawing);
-  // // compare line parameters
-  // let angleDiff = abs(referenceLineParameters[1] - fitLineParameters[1]);
-  // let lengthDiff = abs(referenceLineParameters[2] - fitLineParameters[2]);
-  // let distance = dist(referenceLineParameters[0].x, referenceLineParameters[0].y, fitLineParameters[0].x, fitLineParameters[0].y);
-  // // console.log("angleDiff: " + angleDiff, "lengthDiff: " + lengthDiff, "distance: " + distance);
+    fitToDrawing.updateWithFitToDrawing(drawingPoints);
+
+    // compare the fit to the reference
+    let similarityScores = reference.getSimilarityScores(fitToDrawing, canvasL);
+
+    displayScores(similarityScores);
   }
 }
 
-// // function that finds the linear regression fit parameters a, b for points
-// function linearRegressionFit(points) {
-//   let sumX = 0;
-//   let sumY = 0;
-//   let sumXY = 0;
-//   let sumXX = 0;
-//   let n = points.length;
-//   for (let i = 0; i < n; i++) {
-//     sumX += points[i].x;
-//     sumY += points[i].y;
-//     sumXY += points[i].x * points[i].y;
-//     sumXX += points[i].x * points[i].x;
-//   }
-//   let a = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
-//   let b = (sumY - a * sumX) / n;
-//   return [a, b];
-// }
+function resetScoreDiv() {
+  let scoreDiv = select("#scoreDiv");
+  if (scoreDiv) {
+    scoreDiv.remove();
+  }
+}
 
-// function getLineParameters(inputLine) {
-//   // get center of line
-//   let x = (inputLine[0] + inputLine[2]) / 2;
-//   let y = (inputLine[1] + inputLine[3]) / 2;
-//   // get angle of line
-//   let angle = atan2(inputLine[3] - inputLine[1], inputLine[2] - inputLine[0]);
-//   // convert to degrees
-//   angle = angle * 180 / PI;
-//   // get length of line
-//   let length = dist(inputLine[0], inputLine[1], inputLine[2], inputLine[3]);
-//   return [{x: x, y: y}, angle, length];
-// }
-
+function displayScores(scores) {
+  resetScoreDiv();
+  scoreDiv = createDiv();
+  scoreDiv.position(windowWidth/2 + 10, canvasHeight + 15);
+  scoreDiv.id("scoreDiv");
+  scoreDiv.style("display", "flex");
+  scoreDiv.style("flex-direction", "row");
+  scoreDiv.style("column-gap", ".1em");
+  scoreDiv.style("font-size", "1em");
+  scoreDiv.style("justify-content", "space-between");
+  scoreDiv.style("width",  windowWidth/2 - 20 + "px");
+  for(let key of similarityScoreKeys) {
+    // display the label and score in their own divs so they can be styled separately
+    let div = createDiv(similarityScoreLabels[key]);
+    div.style("color", "#888");
+    scoreDiv.child(div);
+    div = createDiv(scores[key]);
+    div.style("font-weight", "bold");
+    scoreDiv.child(div);
+    // add a separator
+    if (key != similarityScoreKeys[similarityScoreKeys.length - 1]) {
+      scoreDiv.child(createDiv("|"));
+    }
+  }
+}
 
 function drawCanvasBorder(canvas) {
   // draw border around the canvas
   canvas.noFill();
   canvas.stroke(150);
   canvas.strokeWeight(3);
-  canvas.rect(canvaseBorderOffset, canvaseBorderOffset, canvasWidth-2*canvaseBorderOffset, canvasHeight-2*canvaseBorderOffset);
+  canvas.rect(canvaseBorderOffset, canvaseBorderOffset, canvasWidth - 2 * canvaseBorderOffset, canvasHeight - 2 * canvaseBorderOffset);
 }
 
-function drawStudentCanvas(canvas, ref) {
+function drawStudentCanvas(canvas, ref, fit) {
   canvas.background(255);
 
   // Draw the reference line
   if (showReference) {
-    ref.draw(canvas, "compare");
+    ref.draw(canvas, "reference");
   }
+
+  // Draw the fit line
+  // if (showFitToDrawing) {
+  //   fit.draw(canvas, "fit");
+  // }
 
   // Draw the student's drawing
   canvas.noFill();
@@ -156,7 +179,7 @@ function drawStudentCanvas(canvas, ref) {
   // Draw the current point
   canvas.fill(200);
   canvas.noStroke();
-  canvas.ellipse(mouseX-canvasWidth, mouseY, 8, 8);
+  canvas.ellipse(mouseX - canvasWidth, mouseY, 8, 8);
 
   drawCanvasBorder(canvas);
 
@@ -164,8 +187,8 @@ function drawStudentCanvas(canvas, ref) {
 }
 
 function drawReferenceCanvas(canvas, ref) {
-  ref.draw(canvas, "reference");
-  
+  ref.draw(canvas, "normal");
+
   drawCanvasBorder(canvas);
   image(canvas, 0, 0);
 }
@@ -174,7 +197,7 @@ function draw() {
 
   drawReferenceCanvas(canvasL, reference);
 
-  drawStudentCanvas(canvasR, reference);
+  drawStudentCanvas(canvasR, reference, fitToDrawing);
 }
 
 
